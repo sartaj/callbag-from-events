801c79168c502ba9e1b370d381bfce03c47f66e8
Sartaj <sartaj@sartaj.me>
2018-07-24 22:05:36 -0500

    create fromEvents method

--- README.md
+++
@@ -1,2 +0,0 @@
-# callbag-from-events
-Creates a stream from events on a DOM EventTarget or a Node.JS EventEmitter object, or an object that supports event listeners using on/off methods.
---
+++ readme.js
@@ -0,0 +1,60 @@
+
+/**
+ * # callbag-from-events
+ *
+ * Create a Callbag from:
+ * - an object with on/off methods,
+ * - a DOM EventTarget,
+ * - or a Node.JS EventEmitter.
+ * 
+ * ## Example
+ * ```
+ * import fromEvents from 'callbag-from-events'
+ * import { map, filter } from 'callbag-basics'
+ * import WebSocket from 'ws'
+ * import { server } from './server'
+ *
+ * const wss = new WebSocket.Server({ server })
+ * 
+ * const chatReceived = fromEvents(wss, 'chat-received')
+ *   |> filter(message => message.length > 0)
+ *   |> map(message => ({ chat: message })) 
+ * ```
+ */
+
+const pairs = [
+  ['on', 'off'],
+  ['addListener', 'removeListener'],
+  ['addEventListener', 'removeEventListener']
+]
+
+const getEventMethod = target => {
+  for (let i = 0; i < pairs.length; i++) {
+    if (
+      typeof target[pairs[i][0]] === 'function' &&
+      typeof target[pairs[i][1]] === 'function'
+    ) return pairs[i]
+  }
+  throw new Error(
+    "fromEvents can't find an event method pair: " + 
+    "on/off, addListener/removeListener, addEventListener/removeEventListener"
+  )
+}
+
+const fromEvents(target, eventToListen) => (start, sink) => {
+  if (start !== 0) return;
+
+  const [subscriber, unsubscriber] = getEventMethod(target)
+
+  const handler = value => {
+    sink(1, value)
+  }
+
+  sink(0, t => {
+    if(t === 2) target[unsubscriber](eventToListen, handler)
+  })
+
+  target[subscriber](eventToListen, handler)
+}
+
+module.exports = fromEvents
