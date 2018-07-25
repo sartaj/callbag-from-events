524407d3dffa46a0ee90e319c8f0bc8691800af7
Sartaj <sartaj@sartaj.me>
2018-07-24 23:44:52 -0500

    merge function for simplified doc annotation

--- readme.js
+++ readme.js
@@ -1,48 +1,47 @@
 
+
 /**
- * # callbag-from-events
- *
- * Create a Callbag from:
- * - an object with on/off methods,
- * - a DOM EventTarget,
- * - or a Node.JS EventEmitter.
- * 
- * ## Example
- * ```
- * import fromEvents from 'callbag-from-events'
- * import { map, filter } from 'callbag-basics'
- * import WebSocket from 'ws'
- * import { server } from './server'
- *
- * const wss = new WebSocket.Server({ server })
- * 
- * const chatReceived = fromEvents(wss, 'chat-received')
- *   |> filter(message => message.length > 0)
- *   |> map(message => ({ chat: message })) 
- * ```
+  # callbag-from-events
+ 
+  Create a Callbag from:
+  - any object with on/off methods,
+  - a DOM EventTarget,
+  - or a Node.JS EventEmitter.
+
+  ```
+  import fromEvents from 'callbag-from-events'
+  import { serverEvents } from './server'
+  
+  const chatReceived = fromEvents(serverEvents, 'chat-received')
+    |> filter(message => message.length > 0)
+    |> map(message => ({ chat: message })) 
+  ```
  */
-
-const pairs = [
-  ['on', 'off'],
-  ['addListener', 'removeListener'],
-  ['addEventListener', 'removeEventListener']
-]
-
-const getEventMethod = target => {
+const fromEvents = (target, eventToListen) => {
+  const pairs = [
+    ['on', 'off'],
+    ['addListener', 'removeListener'],
+    ['addEventListener', 'removeEventListener']
+  ]
+  
+  // Find which pair of functions this target has
+  let subscriber, unsubscriber;
   for (let i = 0; i < pairs.length; i++) {
     if (
       typeof target[pairs[i][0]] === 'function' &&
       typeof target[pairs[i][1]] === 'function'
-    ) return pairs[i]
+    ) {
+      subscriber = pairs[i][0]
+      unsubscriber = pairs[i][1]
+      break;
+    }
   }
-  throw new Error(
-    "fromEvents can't find an event method pair: " + 
-    "on/off, addListener/removeListener, addEventListener/removeEventListener"
-  )
-}
-
-const fromEvents = (target, eventToListen) => {
-  const [subscriber, unsubscriber] = getEventMethod(target)
+  if(!subscriber || !unsubscriber) {
+    throw new Error(
+      "fromEvents can't find an event method pair: " + 
+      "on/off, addListener/removeListener, addEventListener/removeEventListener"
+    )
+  }
 
   return (start, sink) => {
     if (start !== 0) return;
