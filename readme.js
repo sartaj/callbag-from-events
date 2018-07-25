8e6ee90e3e9865b12e1181e9a50894ae3973ce89
Sartaj <sartaj@sartaj.me>
2018-07-25 00:00:07 -0500

    update docs

--- readme.js
+++ readme.js
@@ -1,21 +1,27 @@
 
 
 /**
-  # callbag-from-events
- 
-  Create a Callbag from:
-  - any object with on/off methods,
-  - a DOM EventTarget,
-  - or a Node.JS EventEmitter.
-
-  ```
-  import fromEvents from 'callbag-from-events'
-  import { serverEvents } from './server'
-  
-  const chatReceived = fromEvents(serverEvents, 'chat-received')
-    |> filter(message => message.length > 0)
-    |> map(message => ({ chat: message })) 
-  ```
+ * # callbag-from-events
+ *
+ * Create a Callbag from:
+ * - an object with on/off methods,
+ * - a DOM EventTarget,
+ * - or a Node.JS EventEmitter.
+ *
+ * 
+ *     import fromEvents from 'callbag-from-events'
+ *     import { map, filter } from 'callbag-basics'
+ *     import { ipcMain } from 'electron'
+ *     import { nodeEvents } from './server'
+ *  
+ *     const chatReceived = fromEvents(nodeEvents, 'chat-received')
+ *       |> filter(message => message.length > 0)
+ *       |> map(message => ({ chat: message }))
+ *     
+ *     const buttonClicked = fromEvents(document.getElementById('start'), 'click')
+ * 
+ *     const processCompleted = fromEvents(ipcMain, 'process-completed')
+ * 
  */
 const fromEvents = (target, eventToListen) => {
   const pairs = [
